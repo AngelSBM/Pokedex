0fee81927355bad859b3551cc3af6f879be8235c
AngelSBM <angelsebastianbellomateo@gmail.com>
2021-01-06 16:56:02 -0400

    Agregando imagen al navbar y dandole algo de estilo, creando las rutas del home y el detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PokeDetailComponent } from './components/poke-detail/poke-detail.component';
+import { PokeHomeComponent } from './components/poke-home/poke-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home', component: PokeHomeComponent },
+  { path:'detail/:id', component: PokeDetailComponent },
+  { path:'**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
