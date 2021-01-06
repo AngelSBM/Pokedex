59a6cabd4377256ff2e719ced9824f17b6a018e2
AngelSBM <angelsebastianbellomateo@gmail.com>
2021-01-06 16:08:04 -0400

    Creación de componentes y paginas de la App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PokeHomeComponent } from './components/poke-home/poke-home.component';
+import { PokeDetailComponent } from './components/poke-detail/poke-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    PokeHomeComponent,
+    PokeDetailComponent
   ],
   imports: [
     BrowserModule,
